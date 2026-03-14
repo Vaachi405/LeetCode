32813c52e4cea863c8f868eb10b5058aa9935a5f
Vaachi405 <159532195+Vaachi405@users.noreply.github.com>
2026-03-14 20:21:59 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0326-power-of-three/0326-power-of-three.java
+++ 0326-power-of-three/0326-power-of-three.java
@@ -1,6 +1,9 @@
-public class Solution {
+class Solution {
     public boolean isPowerOfThree(int n) {
-        int maxPowerOf3 = 1162261467; // 3^19 is the largest power of 3 in int range
-        return n > 0 && maxPowerOf3 % n == 0;
+        if(n<=0) return false;
+        while(n%3==0){
+            n/=3;
+        }
+        return n==1;
     }
 }
