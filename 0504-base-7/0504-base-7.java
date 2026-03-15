324879dc38865d59c4522bc01aa229b55cdbfcb2
Vaachi405 <159532195+Vaachi405@users.noreply.github.com>
2026-03-15 17:48:46 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0504-base-7/0504-base-7.java
+++ 0504-base-7/0504-base-7.java
@@ -1,19 +1,5 @@
 class Solution {
     public String convertToBase7(int num) {
-        if (num == 0) return "0";
-
-        boolean negative = num < 0;
-        num = Math.abs(num);
-
-        StringBuilder result = new StringBuilder();
-
-        while (num > 0) {
-            result.append(num % 7);
-            num /= 7;
-        }
-
-        if (negative) result.append('-');
-
-        return result.reverse().toString();
+        return Integer.toString(num,7);
     }
 }
