117d76e666a7492d546756255a9e49971dba32e0
Vaachi405 <159532195+Vaachi405@users.noreply.github.com>
2026-03-28 12:33:28 +0530

    Time: 0 ms (100%), Space: 43 MB (33.25%) - LeetHub

---
+++ 0213-house-robber-ii/0213-house-robber-ii.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int rob(int[] nums) {
+        int n= nums.length;
+        
+        if(n==1) return nums[0];
+        if(n==2) return Math.max(nums[0],nums[1]);
+        int ans1=solve(nums,0,n-2);
+        int ans2=solve(nums,1,n-1);
+        return Math.max(ans1,ans2);
+    }
+    private int solve(int[] nums,int start, int end){
+        int prev1=0; //store the profit till previous house
+        int prev2=0; //store the profit till previous to previous house
+        for(int i= start; i<=end; i++){
+            int curr= Math.max(prev1, prev2+nums[i]); // select the max profit
+            prev2=prev1;
+            prev1=curr;
+        }
+        return prev1;
+    }
+}
