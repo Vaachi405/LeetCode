511e8d5fafa3e5b1d5b45b5994dad65bb4baf626
Vaachi405 <159532195+Vaachi405@users.noreply.github.com>
2026-04-06 10:30:25 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1341-movie-rating/1341-movie-rating.sql
+++ 1341-movie-rating/1341-movie-rating.sql
@@ -1,28 +1,21 @@
 # Write your MySQL query statement below
 (
-    SELECT u.name AS results
-    FROM users u
-    JOIN movierating m
-        ON u.user_id=m.user_id
-    GROUP BY u.user_id
-    ORDER BY COUNT(u.user_id) DESC, u.name ASC 
-    LIMIT 1
+    Select u.name as results
+    from users u
+    join movierating m
+        on u.user_id=m.user_id
+    group by u.user_id
+    order by count(u.user_id) desc, u.name asc
+    limit 1
 )
-/*First by rating count (DESC) to get user with most ratings
-Then by name (ASC) to get lexicographically smallest in case of tie
-*/
 UNION ALL
 (
-    SELECT mo.title AS results
-    FROM movies mo
-    JOIN movierating m
-        ON mo.movie_id=m.movie_id
-    WHERE m.created_at BETWEEN '2020-02-01' AND '2020-02-29'
-    GROUP BY mo.movie_id, mo.title
-    ORDER BY AVG(m.rating) DESC, mo.title ASC 
-    LIMIT 1
+    select mo.title as results
+    from movies mo
+    join movierating m
+        on mo.movie_id = m.movie_id
+    where m.created_at between '2020-02-01' and '2020-02-29'
+    group by mo.movie_id, mo.title
+    order by avg(m.rating) desc, mo.title asc
+    limit 1
 )
-/*
-First by average rating (DESC) to get highest rated movie
-Then by title (ASC) to get lexicographically smallest in case of tie
-*/
