cca7a43a0e89016f8ff68d612a63c04e71892d1a
Vaachi Goel <159532195+Vaachi405@users.noreply.github.com>
2026-04-25 19:32:09 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.java
+++ 0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.java
@@ -16,23 +16,27 @@
 class Solution {
     public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
         List<List<Integer>> result = new ArrayList<>();
-        if(root==null) return result;
+        if (root == null)
+            return result;
 
         Queue<TreeNode> queue = new LinkedList<>();
         queue.add(root);
-        boolean dir = true;
+        boolean dir = true; //for direction
 
-        while(!queue.isEmpty()){
+        while (!queue.isEmpty()) {
             int size = queue.size();
-            Integer row[] = new Integer[size];
+            Integer row[] = new Integer[size]; //temporary array
 
-            for(int i=0; i<size; i++){
+            for (int i = 0; i < size; i++) {
                 TreeNode node = queue.poll();
-                int index = dir ? i : size-1-i;
+                int index = dir ? i : size - 1 - i; 
+                //to get the index of where to store the value 
                 row[index] = node.val;
 
-                if(node.left!=null) queue.add(node.left);
-                if(node.right!=null) queue.add(node.right);
+                if (node.left != null)
+                    queue.add(node.left);
+                if (node.right != null)
+                    queue.add(node.right);
             }
 
             dir = !dir;
