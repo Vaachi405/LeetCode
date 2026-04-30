b8ad3f028334a7dd51a52e3eff2f3f3002bf1e1c
Vaachi Goel <159532195+Vaachi405@users.noreply.github.com>
2026-04-30 21:31:47 +0530

    Time: 14 ms (22.71%), Space: 44 MB (61.31%) - LeetHub

---
+++ 0863-all-nodes-distance-k-in-binary-tree/0863-all-nodes-distance-k-in-binary-tree.java
@@ -0,0 +1,63 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    private static void markParent(TreeNode root, Map<TreeNode, TreeNode> parent, TreeNode target) {
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+
+        while (!queue.isEmpty()) {
+            TreeNode curr = queue.poll();
+            if (curr.left != null) {
+                parent.put(curr.left, curr);
+                queue.add(curr.left);
+            }
+            if (curr.right != null) {
+                parent.put(curr.right, curr);
+                queue.add(curr.right);
+            }
+        }
+    }
+
+    public List<Integer> distanceK(TreeNode root, TreeNode target, int k) {
+        Map<TreeNode, TreeNode> parent = new HashMap<>();
+        markParent(root, parent, target);
+        Map<TreeNode, Boolean> vis = new HashMap<>();
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(target);
+        vis.put(target, true);
+        int level = 0;
+
+        while(!queue.isEmpty()){
+            int size = queue.size();
+            if(level==k) break;
+            level++;
+            for(int i=0; i<size; i++){
+                TreeNode current = queue.poll();
+                if(current.left != null && vis.get(current.left)==null){
+                    queue.add(current.left);
+                    vis.put(current.left, true);
+                }
+                if(current.right != null && vis.get(current.right)==null){
+                    queue.add(current.right);
+                    vis.put(current.right, true);
+                }
+                if(parent.get(current) != null && vis.get(parent.get(current))==null){
+                    queue.add(parent.get(current));
+                    vis.put(parent.get(current), true);
+                }
+            }
+        }
+        List<Integer> result = new ArrayList<>();
+        while(!queue.isEmpty()){
+            result.add(queue.poll().val);
+        }
+        return result;
+    }
+}
