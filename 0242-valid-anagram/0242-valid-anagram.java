5331a15144021f23b262c9d01150a091d85383ac
Vaachi Goel <159532195+Vaachi405@users.noreply.github.com>
2026-09-12 12:05:51 +0530

    Time: 16 ms (21.05%), Space: 46.4 MB (41.74%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,9 +1,18 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-        char[] sChars = s.toCharArray();
-        char[] tChars = t.toCharArray();
-        Arrays.sort(sChars);
-        Arrays.sort(tChars);
-        return Arrays.equals(sChars, tChars);
+        if(s.length()!=t.length()) return false;
+        Map<Character, Integer> map= new HashMap<>();
+
+        for(char ch:s.toCharArray()){
+            map.put(ch, map.getOrDefault(ch,0)+1);
+        }
+
+        for(char ch:t.toCharArray()){
+            if(!map.containsKey(ch) || map.get(ch)==0){
+                return false;
+            }
+            map.put(ch, map.get(ch)-1);
+        }
+        return true;
     }
 }
